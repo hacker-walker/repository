6a2bf44e544431c8b0b782dc46d29dca81a13f3d
walker <45536824+liu777@users.noreply.github.com>
2019-06-29 14:20:49 +0800

    repository——》知识宝库创建：LuKe(walker) :fire:

---
+++ src/main/java/com/walker/repository/App.java
@@ -0,0 +1,22 @@
+package com.walker.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: repository
+ * @EnglishName LuKe
+ * @authod LiuQi
+ * @date 2019/6/29 14:12
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class, args);
+
+        System.out.println("2019年，祝每一个努力的人都有所收获");
+    }
+
+}
